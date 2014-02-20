b8db11b8dac7fca70d3af5ebee9650d64c1345f1
aliyanno <aliyamarder@gmail.com>
2014-02-19 19:40:56 -0800

    Style changes, new empty task list function

--- js/todo-list.js
+++ js/todo-list.js
@@ -3,7 +3,7 @@ var toDoApp = angular.module("toDoApp", []);
 
 // Define the controller in the angular module
 toDoApp.controller("toDoController", ["$scope", function($scope) {
-	$scope.date = new Date();
+	$scope.empty = ("All done for the day! See whats on the agenda tomorrow...");
 	$scope.tasks = [
 		{"name": "Feed the dogs", "done": false}, 
 		{"name": "Take the garbage out", "done": false},
@@ -15,9 +15,15 @@ toDoApp.controller("toDoController", ["$scope", function($scope) {
 		$scope.tasks = _.filter($scope.tasks, function(task) {
 			return !(task.done);
 		});
+		if ($scope.tasks == []) {
+
+		}
 	}
 	$scope.addTask = function(arg) {
 		$scope.tasks.push({"name": arg, "done": false});
 		$scope.newTaskName = "";
 	}	
+	$scope.isEmpty = function() {
+		return $scope.getTotalTasks() == 0;
+	}
 }]);
