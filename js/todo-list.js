1d91ea94f4791cb164c344105001d54ff9fecb27
aliyanno <aliyamarder@gmail.com>
2014-02-19 16:46:42 -0800

    Fixes to Task Counter

--- js/todo-list.js
+++ js/todo-list.js
@@ -3,12 +3,14 @@ var toDoApp = angular.module("toDoApp", []);
 
 // Define the controller in the angular module
 toDoApp.controller("toDoController", ["$scope", function($scope) {
-	$scope.totalTasks = 4;
 	$scope.date = new Date();
 	$scope.tasks = [
 		{"name": "Feed the dogs", "done": false}, 
 		{"name": "Take the garbage out", "done": false},
 	];
+	$scope.getTotalTasks = function() {
+		return $scope.tasks.length;
+	}
 	$scope.clearCompleted = function() {
 		$scope.tasks = _.filter($scope.tasks, function(task) {
 			return !(task.done);
